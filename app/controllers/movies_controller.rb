b2713c7c156867c4a6d884e3bc59d6b39dc12bc2
Joel Sommers <jsommers@colgate.edu>
2013-02-05 14:06:30 -0500

    Add show action in controller, and appropriate view

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,4 +2,10 @@ class MoviesController < ApplicationController
   def index
     @movies = Movie.all
   end
+
+  def show
+    id = params[:id]
+    @movie = Movie.find(id)
+    # will render app/views/movies/show.html.haml by default
+  end
 end
