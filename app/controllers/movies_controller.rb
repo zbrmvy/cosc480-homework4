9a3d139b35271783b13d6ac828fbb31bace6fdcc
Joel Sommers <jsommers@colgate.edu>
2013-02-05 14:11:29 -0500

    Add remaining actions and view content to movies

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,3 +1,5 @@
+# in app/controllers/movies_controller.rb
+
 class MoviesController < ApplicationController
   def index
     @movies = Movie.all
@@ -7,5 +9,33 @@ class MoviesController < ApplicationController
     id = params[:id]
     @movie = Movie.find(id)
     # will render app/views/movies/show.html.haml by default
+  end
+
+  def new
+    # default: render 'new' template
+  end
+
+  def create
+    @movie = Movie.create!(params[:movie])
+    flash[:notice] = "#{@movie.title} was successfully created."
+    redirect_to movies_path
+  end
+
+  def edit
+    @movie = Movie.find params[:id]
+  end
+
+  def update
+    @movie = Movie.find params[:id]
+    @movie.update_attributes!(params[:movie])
+    flash[:notice] = "#{@movie.title} was successfully updated."
+    redirect_to movie_path(@movie)
+  end
+
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    flash[:notice] = "Movie '#{@movie.title}' deleted."
+    redirect_to movies_path
   end
 end
