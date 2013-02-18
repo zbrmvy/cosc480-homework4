609b839d6f63a5e9f1f9066d572a182b10ae22a3
Joel Sommers <jsommers@colgate.edu>
2013-02-18 14:59:31 -0500

    Update paths.rb to get add movie scenario to pass

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -13,8 +13,9 @@ module NavigationHelpers
   def path_to(page_name)
     case page_name
 
-    when /^the (RottenPotatoes )?home\s?page$/ then '/movies'
-    when /^the movies page$/ then '/movies'
+     when /^the (RottenPotatoes )?home\s?page$/ then '/movies'
+     when /^the movies page$/ then '/movies'
+     when /^the Create New Movie page$/ then '/movies/new'
 
     # Add more mappings here.
     # Here is an example that pulls values out of the Regexp:
